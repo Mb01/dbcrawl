0284a4a7d428bf7c416546bd1f620b0bca3c1041
Mark Bowden <Mbowden01@gmail.com>
2013-11-27 22:33:49 +0900

    Update crawl.py
    commented dubious line

--- crawl.py
+++ crawl.py
@@ -86,7 +86,7 @@ while True:
                 if not href: continue
                 try:
                     if href[-1] == '/':
-                        href = href[:-1]
+                        href = href[:-1]#WAIT< THAT DOES NOTHING, RIGHT!?
                     href = href.encode('utf-8')
                     if str(href).find('http') == 0:
                         pass                        
